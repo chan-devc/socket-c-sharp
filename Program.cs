bcf2971e21575aa5413fea683e56dd8afb924f3d
chan-devc <chansamone4792@gmail.com>
2023-09-21 10:14:19 +0700

    Socket C#

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add("http://192.168.1.123:8080/");
+        listener.Start();
+        Console.WriteLine("WebSocket server listening on http://localhost:8080/");
+
+        while (true)
+        {
+            HttpListenerContext context = await listener.GetContextAsync();
+            if (context.Request.IsWebSocketRequest)
+            {
+                ProcessWebSocketRequest(context);
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+                context.Response.Close();
+            }
+        }
+    }
+
+    static async void ProcessWebSocketRequest(HttpListenerContext context)
+    {
+        WebSocketContext webSocketContext = await context.AcceptWebSocketAsync(null);
+        WebSocket webSocket = webSocketContext.WebSocket;
+
+        byte[] buffer = new byte[1024];
+        while (webSocket.State == WebSocketState.Open)
+        {
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            if (result.MessageType == WebSocketMessageType.Text)
+            {
+                string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                Console.WriteLine($"Received: {message}");
+
+                // Echo the received message back to the client
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+            else if (result.MessageType == WebSocketMessageType.Close)
+            {
+                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+            }
+        }
+    }
+}
